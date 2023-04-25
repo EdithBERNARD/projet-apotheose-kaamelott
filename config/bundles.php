85ddf2c09b827cd527c03cab9850f1242f8765df
Angèle LEGALL <angie301@hotmail.fr>
2023-04-25 17:42:45 +0200

    delete nelmio api doc  bundle

--- config/bundles.php
+++ config/bundles.php
@@ -13,5 +13,4 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
-    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
